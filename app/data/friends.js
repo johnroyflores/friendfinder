5cb58551c06224c73cc7ac1f04e371ee8b05f6e4
John Flores <johnroyflores@gmail.com>
2016-07-23 14:04:00 -0500

    corrected the routing and added class code for survey as starting point

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [
+	{
+		name: 'Donald Trump',
+		photo: 'http://i2.cdn.turner.com/cnnnext/dam/assets/160118134132-donald-trump-nigel-parry-large-169.jpg',
+		scores: [1, 1, 1,1,1,1,1,1,1,1]
+	},{
+		name: 'Killer Mike',
+		photo: 'http://media.npr.org/assets/img/2012/06/18/killermike3_wide-0ec13ecf1b337ec043db1a87755f704c69fe918d-s900-c85.jpg',
+		scores: [5, 5, 5,5,5,5,5,5,5,5]
+	},{
+		name: 'Tupac',
+		photo: 'http://cp91279.biography.com/1000509261001/1000509261001_2050821238001_Bio-Biography-Tupac-Shakur-SF.jpg',
+		scores: [2,2,2,2,2,2,2,2,2,2]
+	}];
+
+module.exports = friends;
